12c9b49022a5d3e296ebe558c0da23b4ca12ddab
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-17 17:50:13 +0300

    Add dark them and create login form

---
+++ lib/notifiers/DarkThemeProvider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:todo_app/preferences/DarkThemePreference.dart';
+
+class DarkThemeProvider with ChangeNotifier {
+  DarkThemePreference darkThemePreference = DarkThemePreference();
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
+
+  set darkTheme(bool value) {
+    _darkTheme = value;
+    darkThemePreference.setDarkTheme(value);
+    notifyListeners();
+  }
+}
