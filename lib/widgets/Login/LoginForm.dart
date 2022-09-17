91d50107d88dd64f70ec652385a88dc13868d106
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-17 17:50:49 +0300

    add login validation

---
+++ lib/widgets/Login/LoginForm.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/notifiers/DarkThemeProvider.dart';
+import 'package:todo_app/widgets/Todos/TodoList.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  LoginState createState() {
+    return LoginState();
+  }
+}
+
+class LoginState extends State<LoginForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Login"),
+            actions: [Row(
+                mainAxisAlignment: MainAxisAlignment.center,children: [
+                  // show Dark icon when dark theme is enabled and vice versa
+                  Icon(!themeChange.darkTheme ? Icons.wb_sunny : Icons.nightlight_round),
+              Switch(
+                value: themeChange.darkTheme,
+                onChanged: (bool value) {
+                  themeChange.darkTheme = value;
+                },
+              )
+              ])
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+                  child: Center(
+                    child: SizedBox(
+                        width: 200,
+                        height: 150,
+                        child: Image.asset('assets/images/todo.jpg')),
+                  ),
+                ),
+                Padding(
+                  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: TextFormField(
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email address';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter valid email id as abc@gmail.com')
+                    ,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+                  //padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter secure password')
+                    ,
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){
+                    //TODO FORGOT PASSWORD SCREEN GOES HERE
+                  },
+                  child: const Text(
+                    'Forgot Password',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(context, MaterialPageRoute(builder: (_) => const TodoList()));
+                      }
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 130,
+                ),
+                const Text('New User? Create Account')
+              ],
+            ),
+          ),
+        )
+      ,
+    );
+  }
+}
